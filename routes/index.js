794397fe7073546ee9ef2cfa2680564fc8f7add7
kh.jung <kh.jung@eggbun-edu.com>
2017-10-29 17:01:52 +0900

    Initial commit
    We can see data of notifications from mbed callback

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/mbed-callback', (req, res, next) => {
+  console.log(req.body);
+  console.log(req.data);
+  res.status(200).send('ok');
+});
+
+router.put('/mbed-callback', (req, res, next) => {
+  console.log(req.body);
+  try {
+    const payload = req.body['async-responses'][0].payload
+    var b = new Buffer(payload, 'base64')
+    var s = b.toString();
+    console.log(s);
+  } catch (e) {}
+
+  try {
+    const payload = req.body['notifications'][0].payload;
+    var b = new Buffer(payload, 'base64');
+    var s = b.toString();
+    console.log(s);
+  } catch (e) {}
+  res.status(200).send('ok');
+});
+
+module.exports = router;
