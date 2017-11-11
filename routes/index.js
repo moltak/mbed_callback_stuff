419ecdb3c16e9a132a1d12f3d0231a8bd2f01a13
kh.jung <kh.jung@eggbun-edu.com>
2017-11-12 01:08:41 +0900

    start to write mbed-callback functions

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+/*eslint-disable no-alert, no-console */
 var express = require('express');
 var router = express.Router();
 
@@ -7,27 +8,42 @@ router.get('/', function(req, res) {
 });
 
 router.get('/mbed-callback', (req, res) => {
-  console.log(req.body);
-  console.log(req.data);
+  console.log('-------');
+  console.log('body: %j', req.body);
+  console.log('query: %j', req.query);
+  console.log('-------');
   res.status(200).send('ok');
 });
 
 router.put('/mbed-callback', (req, res) => {
-  console.log(req.body);
-  try {
-    const payload = req.body['async-responses'][0].payload
-    var b = new Buffer(payload, 'base64')
-    var s = b.toString();
-    console.log(s);
-  } catch (e) {}
-
-  try {
-    const payload = req.body['notifications'][0].payload;
-    var b = new Buffer(payload, 'base64');
-    var s = b.toString();
-    console.log(s);
-  } catch (e) {}
+  printMbedParams(req.body);
   res.status(200).send('ok');
 });
+
+function printMbedParams(body) {
+  console.log('-------');
+  console.log('%j', body);
+  let base64Payload;
+
+  if (body['async-responses']) {
+    base64Payload = body['async-responses'][0].payload;
+  }
+  
+  if (body['notifications']) {
+    base64Payload = body['notifications'][0].payload;
+  }
+
+  if (base64Payload) {
+    const payload = extractPayload(base64Payload);
+    console.log(payload);
+  }
+
+  console.log('-------');
+}
+
+function extractPayload(payload) {
+  var buffer = new Buffer(payload, 'base64');
+  return buffer.toString();
+}
 
 module.exports = router;
