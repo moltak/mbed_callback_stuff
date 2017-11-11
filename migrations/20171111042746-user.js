c76320764b597381ca1db62ef82bb664133f2181
kh.jung <kh.jung@eggbun-edu.com>
2017-11-11 13:41:17 +0900

    insert fake user

---
+++ migrations/20171111042746-user.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      fingerId: {
+        type: Sequelize.STRING
+      },
+      famaily: {
+        type: Sequelize.ARRAY
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      sns: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
--- service/user/UserRoute.js
+++ service/user/UserRoute.js
@@ -1,16 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+const fakeUser = {
+  firstName: 'Luis',
+  lastName: 'Ko',
+  fingerId: 'fingerId1',
+  famaily: 'family1',
+  phone: '01012345678',
+  sns: 'kakaotalk id?'
+};
+
 router.get('/', function(req, res) {
   res.status(200).json({
-    user: {}
+    user: fakeUser
   });
 });
 
 router.get('/all', function(req, res) {
   res.status(200).json({
     users: [
-      {}
+      fakeUser
     ]
   });
 });
@@ -18,7 +27,7 @@ router.get('/all', function(req, res) {
 router.get('/relationship', function(req, res) {
   res.status(200).json({
     users: [
-      {}
+      fakeUser
     ]
   });
 });
